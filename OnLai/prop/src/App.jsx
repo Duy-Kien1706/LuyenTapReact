be0ec6735a98c247246212aee7baee781f727882
Duy-Kien1706 <duykien170606@gmail.com>
2026-06-04 10:09:41 +0700

    Ôn lại prop

---
+++ OnLai/prop/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Students from './components/Students'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Students name = "Nguyễn Duy Kiên" age = {20} major = "Công nghệ thông tin"> </Students>
+    </>
+  )
+}
+
+export default App
---
+++ OnLai/prop/src/components/Students.jsx
@@ -0,0 +1,9 @@
+export default function Students({name, age, major}){
+    return (
+        <>
+            <h1>Tên: {name}</h1>
+            <h1>Tuổi: {age}</h1>
+            <h1>Ngành: {major}</h1>
+        </>
+    )
+}
